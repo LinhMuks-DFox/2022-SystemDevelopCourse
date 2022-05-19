bef3321913a7c56d43ccb72e0afdbf8c28c73461
dotFoxyyyy <muxxum65536@gmail.com>
2022-05-19 21:52:03 +0900

    doing Implementaion

--- 2-Othello/mainwindow.py
+++ 2-Othello/mainwindow.py
@@ -1,6 +1,6 @@
+from enum import Enum
 import sys
 from PySide6.QtWidgets import QApplication, QMainWindow
-from PySide6.QtCore import QFile
 from WindowsGUI import Ui_MainWindow
 import PySide6
 import os
@@ -11,12 +11,33 @@ plugin_path = os.path.join(dirname, 'plugins', 'platforms')
 os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = plugin_path
 
 
+class Color(Enum):
+    Black = 0
+    White = 1
+    NotSetted = 2
+
+
 class OthelloGame:
     def __init__(self):
         self.check_board = [[None for _ in range(8)] for _ in range(8)]
 
-    def change_piece(self, position: str):
-        print(position)
+    def change_piece(self, position: str, color: Color) -> bool:
+        i, j = self._get_indexes(position)
+        if self.check_board[i][j] is not None:
+            return False
+        else:
+            self.check_board[i][j] = color
+            return True
+
+    def get_color(self, pos: str) -> Color:
+        i, j = self._get_indexes(pos)
+        return self.check_board[i][j]
+
+    @staticmethod
+    def _get_indexes(position: str):
+        if len(position) > 2:
+            raise ValueError(f"Argument {position} is invalid.")
+        return int(position[1]), "abcdefg".index(position[0])  # (row, col)
 
 
 class MainWindow(QMainWindow):
@@ -25,26 +46,28 @@ class MainWindow(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.othello = OthelloGame()
-        self.piece_bottons = [
+        self.piece_buttons = [
             getattr(self.ui, attrname) for attrname in [
                 f"{alpha}{num}" for alpha in "abcdefgh" for num in range(1, 9)
             ]
         ]
-        for pb in self.piece_bottons:
+        # set piece_buttons's click function
+        for pb in self.piece_buttons:
             pb.clicked.connect(
-                (lambda _=None, pb=pb: self.othello.change_piece(
+                (lambda _=None, pb=pb: self.click(
                     pb.objectName()))
             )
+        self.ui.reset_button.clicked.connect(self.reset_ui)
 
-        self.ui.reset_button.clicked.connect(self.reset)
-        self.show()
-
-    def reset(self):
+    def reset_ui(self):
         self.ui.GameInfoLabel.setText("Reset game!")
         print("reset_button pressed.")
+
+    def click(self, pos: str):
+        self.othello.change_piece(pos, Color.Black)
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = MainWindow()
+    MainWindow().show()
     sys.exit(app.exec())
