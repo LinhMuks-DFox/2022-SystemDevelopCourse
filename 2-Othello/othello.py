8937c3e3cdbdea9f73aaf3853a04f126a1e4501b
dotFoxyyyy <muxxum65536@gmail.com>
2022-05-19 13:16:56 +0900

    UI Done script coding

--- 2-Othello/othello.py
+++ 2-Othello/othello.py
@@ -5,13 +5,16 @@ try:
     from PyQt5 import uic
 except ImportError as imper:
     print("PyQt5 is not usable, use this program after install it.")
+    exit(-1)
+from cProfile import label
+from dataclasses import dataclass
 import sys
 
-try:
-    import os
-    assert os.path.exists("main.ui")
-except AssertionError:
-    print("Cannot find ui file.")
+
+@dataclass
+class PiecesButton:
+    button: QPushButton
+    name: str
 
 
 class UI(QMainWindow):
@@ -20,34 +23,38 @@ class UI(QMainWindow):
         super(UI, self).__init__()
         # Loading UI File
         uic.loadUi("main.ui", self)
-
-        self._counter = 0  # 0 and 1 repr whose turn
-        self.checkerboard = [
-            None for _ in range(9)
-        ]
+        self._checkerboard = [[None for _ in range(8)] for _ in range(8)]
 
         self.pieces_button = [
-            self.findChild(QPushButton, f"{i}")
-            for i in [f"{x}{y}"for x in "abcdefgh" for y in range(1, 9)]
+            self.findChild(QPushButton, i)
+            for i in [
+                f"{x}{y}" for x in "abcdefgh" for y in range(1, 9)
+            ]
         ]
-        self.reset_button = self.findChild(QPushButton, "reset_button")
-        # Defining widgets
-        self.game_info_label = self.findChild(QLabel, "GameInforLabel")
 
-        # Click the button
+        self.reset_button = self.findChild(QPushButton, "reset_button")
+        self.game_info_label = self.findChild(QLabel, "GameInfoLabel")
+
         # self.button{number}.clicked.connect(lambda: self.clicker(self.button9))
         self.reset_button.clicked.connect(self.reset)
+
         # Show The App
         self.show()
 
-    def clicker(self, b: QPushButton):
-        pass
+    def clicker(self, b):
+        print(b.name)
 
     def reset(self):
-        pass
+        print(type(self.reset_button))
 
 
 if __name__ == "__main__":
+    try:
+        import os
+        assert os.path.exists("main.ui")
+    except AssertionError:
+        print("Cannot find ui file.")
+        exit(-1)
     app = QApplication(sys.argv)
     UIWindow = UI()
     app.exec_()
