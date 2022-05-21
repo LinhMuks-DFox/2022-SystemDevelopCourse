3144a14f08f0e19dd2539c3bb7316cd3855266c6
dotFoxyyyy <muxxum65536@gmail.com>
2022-05-22 00:01:25 +0900

    Building game

--- 2-Othello/othellogame.py
+++ 2-Othello/othellogame.py
@@ -21,9 +21,9 @@ if platform.system() == "Windows":
 
 # Pragma region Constants
 PIECE_BUTTON_DEFAULT_STYLE_SHEET = "background-color: light gray;"
-PIECE_BUTTON_BLACK_STYLE_SHEET = "background-color: #FFFFFF; color: #000000"  # set font color to white
-PIECE_BUTTON_WHITE_STYLE_SHEET = "background-color: #000000; color: #FFFFFF"
-PIECE_BUTTON_VALID_PLACE_STYLE_SHEET = "background-color: #66CCFF; color: #FFFFFF"
+PIECE_BUTTON_BLACK_STYLE_SHEET = "background-color: black; color: white"  # set font color to white
+PIECE_BUTTON_WHITE_STYLE_SHEET = "background-color: white; color: black"
+PIECE_BUTTON_VALID_PLACE_STYLE_SHEET = "background-color: #66CCFF; color: valid"
 DEBUG = True
 
 
@@ -81,11 +81,14 @@ class OthelloGame(QMainWindow):
         self.ui.TotalCounter.setText(f"Total: {self.pressed_button_cnt}")
         self.ui.BlackPieceCounter.setText(f"Black: {2}")
         self.ui.WhitePieceCounter.setText(f"White: {2}")
-        self._rander_valid_place(Color.Black)
+        # self._rander_valid_place(Color.Black)
+
     def _pb_enable(self, b):
         def _able(pb: QPushButton):
             pb.setEnabled(b)
+
         self._foreach_piece_button(_able)
+
     def _click(self, pos: str):
 
         # TODO: Check if piece is placed at a valid position
@@ -95,7 +98,6 @@ class OthelloGame(QMainWindow):
 
         # Set piece
         self._set_piece(pos, self._current_player)
-
         # check if winner occurred
         if 64 - self.pressed_button_cnt == 0:
             self.ui.GameInfoLabel.setText(f"Winner is: {self._check_winner()}!")
@@ -130,30 +132,22 @@ class OthelloGame(QMainWindow):
         '''
         # For flip color:
         i, j = self._get_indexes(position)
-        flip = False
         if self._game_check_board[i][j] is Color.NotSet:
             self._game_check_board[i][j] = color
         else:
             self._game_check_board[i][j] = Color.Black if color == Color.White else Color.White
-            flip = True
 
         # Set UI after click
         cur_pb = getattr(self.ui, position)
-        if not flip:
-            if color == Color.Black:
-                cur_pb.setText("B")
-                cur_pb.setStyleSheet(PIECE_BUTTON_BLACK_STYLE_SHEET)
-            else:
-                cur_pb.setText("W")
-                cur_pb.setStyleSheet(PIECE_BUTTON_WHITE_STYLE_SHEET)
-            self.pressed_button_cnt += 1
+        if color == Color.Black:
+            cur_pb.setText("B")
+            cur_pb.setStyleSheet(PIECE_BUTTON_BLACK_STYLE_SHEET)
         else:
-            if color == Color.Black:
-                cur_pb.setText("W")
-                cur_pb.setStyleSheet(PIECE_BUTTON_WHITE_STYLE_SHEET)
-            else:
-                cur_pb.setText("B")
-                cur_pb.setStyleSheet(PIECE_BUTTON_BLACK_STYLE_SHEET)
+            cur_pb.setText("W")
+            cur_pb.setStyleSheet(PIECE_BUTTON_WHITE_STYLE_SHEET)
+
+        self.pressed_button_cnt += 1
+        cur_pb.setEnabled(False)
 
     @staticmethod
     def _get_indexes(position: str) -> tuple:
@@ -192,15 +186,60 @@ class OthelloGame(QMainWindow):
     def _check_winner(self) -> Color:
         return Color.Black if self._get_piece_count(Color.Black) > self._get_piece_count(Color.White) else Color.White
 
+    def _get_colors(self, target: Color):
+        ret = []
+        for i, r in enumerate( self._game_check_board):
+            for j, c in enumerate(r):
+                if c == target:
+                    ret.append(f"{'abcdefgh'[i]}{j}")
+
+        return ret
+
+    # TODO: Complete this method
+    """
+    找到每一个棋子的周围的位置s，对每一个位置进行一次是否可以放入棋子的判断
+    可以就放入返回的列表中
+    """
     def _get_valid_place(self, color: Color) -> list:
-        return [f"{x}{y}" for x in "abcdefgh" for y in range(1, 9)]
+        valid_place = list()
+        placed_color = self._get_colors(color)
+        for pc in placed_color:
+             for around in self._get_arounds(pc):
+                 # TODO: check around is valid.
+                 pass
+        return valid_place
+
+    # TODO: Complete this method
+    def _check_color_flip(self, pos):
+        pass
+
+    def _get_arounds(self, pos: str) -> list:
+        r, c = pos[1], pos[0]
+        if pos in ["a1", "a8", "h1", "h8"]:
+            return {
+                "a1": ["a3", "c1"],
+                "a8": ["a6", "c8"],
+                "h1": ["f1", "h3"],
+                "h8": ["f8", "h6"]
+            }[pos]
+        around = []
+        if r in "ab":
+            around.append("")
+
+        if r in "gh":
+            pass
+        around.append(f"{'abcdefgh'['abcdefgh'.index(r) - 2]}{c}")
+        around.append(f"{'abcdefgh'['abcdefgh'.index(r) + 2]}{c}")
+        return around
 
     def _rander_valid_place(self, color: Color):
         def bunt_rander(pb: QPushButton):
-            pass
-            # pb.setStyleSheet(PIECE_BUTTON_VALID_PLACE_STYLE_SHEET)
+            pb.setStyleSheet(PIECE_BUTTON_VALID_PLACE_STYLE_SHEET)
+            pb.setText("VALID")
+
         self._foreach_piece_button(bunt_rander, btn_list=[getattr(self.ui, name)
                                                           for name in self._get_valid_place(color)])
+
 
 if __name__ == "__main__":
     if sys.version_info[1] < 8:
