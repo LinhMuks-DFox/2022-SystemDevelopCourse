eff5bd5d0fc07a63978e1f70bd16f121cbf7fe0f
dotFoxyyyy <muxxum65536@gmail.com>
2022-06-25 12:30:17 +0900

    Dijstra get_path done

--- 5-Dijkstra-Algorithm/Dijstra.py
+++ 5-Dijkstra-Algorithm/Dijstra.py
@@ -1,4 +1,13 @@
+from dataclasses import dataclass
+from queue import PriorityQueue
+
 import WeightedGraph
+
+
+@dataclass(order=True)
+class Node:
+    dis: int
+    v: int
 
 
 class Dijstra:
@@ -7,24 +16,27 @@ class Dijstra:
         self.G = G
         self.G.validate_vertex(s)
         self.s = s
-        self.dis = [float("inf") for _ in range(self.G.V)]  # int array
+        self.dis = [65536 for _ in range(self.G.V)]  # int array
+        self.pre = [-1 for _ in range(self.G.V)]
         self.dis[s] = 0
+        self.pre[s] = s
         self.visited = [False for _ in range(self.G.V)]  # boolean array
 
-        while True:
-            cur_dis = float("inf")
-            cur = -1
-            for v in range(self.G.V):
-                if self.visited[v] is False and self.dis[v] < cur_dis:
-                    cur_dis = self.dis[v]
-                    cur = v
-            if cur == -1:
-                break
+        pq = PriorityQueue()
+        pq.put_nowait(Node(v=s, dis=0))
+
+        while pq.empty() is False:
+            cur = pq.get_nowait().v
+            if self.visited[cur]:
+                continue
             self.visited[cur] = True
+
             for w in self.G.adj_map(cur):
                 if self.visited[w] is False:
                     if self.dis[cur] + self.G.weight_of(cur, w) < self.dis[w]:
                         self.dis[w] = self.dis[cur] + self.G.weight_of(cur, w)
+                        pq.put_nowait(Node(v=w, dis=self.dis[w]))
+                        self.pre[w] = cur
 
     def is_connected_to(self, v: int):
         self.G.validate_vertex(v)
@@ -33,3 +45,15 @@ class Dijstra:
     def dis_to(self, v: int):
         self.G.validate_vertex(v)
         return self.dis[v]
+
+    def get_path(self, t: int)->list:
+        ret = []
+        if self.is_connected_to(t) is False:
+            return ret
+        cur = t
+        while cur != self.s:
+            ret.append(cur)
+            cur = self.pre[cur]
+        ret.append(self.s)
+        ret.reverse()
+        return ret
--- 5-Dijkstra-Algorithm/main.py
+++ 5-Dijkstra-Algorithm/main.py
@@ -4,3 +4,5 @@ g = WeightedGraph.WeightedGraph("graph.txt")
 dis = Dijstra.Dijstra(g, 0)
 for v in range(g.V):
     print(dis.dis_to(v), " ")
+
+print("->".join([str(i) for i in dis.get_path(3)]))
